b21ea56cca7cff60c225a14b437a8345a838f6c4
mmsadek96 <mmsadek96@gmail.com>
2025-04-14 13:17:33 +0200

    Delete databasemakerv5 directory

--- databasemakerv5/server/api/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-from server.api.routes import stocks, indicators, options, correlation, transcripts, settings
-
-__all__ = ["stocks", "indicators", "options", "correlation", "transcripts", "settings"]
